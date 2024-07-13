6a536c09b424cc21c5fe2a33e667d2ad9f188a2d
Robel Fekadu <56929107+robavelii@users.noreply.github.com>
2024-07-13 23:07:50 +0300

    Update github.ts
    random fetch

--- lib/github.ts
+++ lib/github.ts
@@ -29,9 +29,24 @@ export const fetchOpenSourceTools = async (
   page: number = 1,
   perPage: number = 10
 ): Promise<GitHubRepository[]> => {
+
+  // Generate a random date within the last year
+  const endDate = new Date();
+  const startDate = new Date(
+    endDate.getFullYear() - 1,
+    endDate.getMonth(),
+    endDate.getDate()
+  );
+  const randomDate = new Date(
+    startDate.getTime() +
+      Math.random() * (endDate.getTime() - startDate.getTime())
+  );
+  const formattedDate = randomDate.toISOString().split("T")[0];
+
+  
   const response = await axios.get(GITHUB_API_URL, {
     params: {
-      q: query,
+      q: `${query} created:>${formattedDate}`,
       per_page: perPage,
       page: page,
       sort: "stars",
